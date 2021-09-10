b6f27daf1755e7d4cd8e4333856f77a69134afd9
Dhruv Bansal <bansaldhruv1973@gmail.com>
2021-09-10 15:44:08 +0530

    :100:

---
+++ Codeforces_round#724/diluc_and_kaeya.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstdio>
+#include <stack>
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <functional>
+#include <numeric>
+using namespace std;
+#define int long long 
+#define vi vector<int>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define tester int t;cin>>t;while(t--)
+#define looper(a,b,c) for(int a=b;a<c;a++)
+#define ss second
+#define pqmax priority_queue<int>
+#define pqmin priority_queue<int,vi,greater<int> >
+#define mod 1000000007
+#define vs vector<string>
+#define mp map<int,int>
+#define ump unordered_map<int,int>
+#define space ' '
+#define invi(n) vi arr(n); for(int i=0; i<n; i++) cin >> arr[i];
+#define stc stack<char>
+#define sti stack<int>
+#define all(arr) arr.begin(),arr.end()
+#define looprev(i,a,b) for(int i=a;i>=(b);i--)
+#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<space;std::cout<<std::endl;
+#define si set<int>
+int gCd(int a,int b){
+    if(b==0)
+        return a;
+    return gCd(b,a%b);
+}
+int fact(int n){
+    int res=1;
+    for(int i=2;i<=n;i++)
+        res = (res*i)%mod;
+    return res;
+}
+int nCr(int n,int r){
+    return fact(n)/ (fact(r)*fact(n-r));
+}
+    
+bool compare(pair<int,int> &one, pair<int,int> &two){
+    return one.second < two.second;
+}
+int32_t main(){
+    tester{
+        map<pii,int> m;
+        int n;cin>>n;
+        string s;cin>>s;
+        int d_cnt=0,k_cnt=0;
+        looper(i,0,n){
+            if(s[i]=='D')
+                d_cnt++;
+            else 
+                k_cnt++;
+            int temp1=d_cnt,temp2=k_cnt;
+            int hihi = gCd(temp1,temp2);
+            temp1/=hihi,temp2/=hihi;
+            if(temp1==0){
+                m[{0,1}]++;
+                cout<<m[{0,1}]<<space;
+            }else if(temp2==0){
+                m[{1,0}]++;
+                cout<<m[{1,0}]<<space;
+            }else{
+                m[{temp1,temp2}]++;
+                cout<<m[{temp1,temp2}]<<space;
+            }
+        }
+        cout<<endl;
+    }   
+    return 0;
+}
