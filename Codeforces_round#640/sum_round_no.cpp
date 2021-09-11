55745de6ce25b8dcb0973471dbc3651318d64a12
Dhruv Bansal <bansaldhruv1973@gmail.com>
2021-09-12 02:31:59 +0530

    :+1:

---
+++ Codeforces_round#640/sum_round_no.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cstdio>
+#include <stack>
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <functional>
+#include <numeric>
+using namespace std;
+#define int long long 
+#define vi vector<int>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define tester int t;cin>>t;while(t--)
+#define looper(a,b,c) for(int a=b;a<c;a++)
+#define ss second
+#define pqmax priority_queue<int>
+#define pqmin priority_queue<int,vi,greater<int> >
+#define mod 1000000007
+#define vs vector<string>
+#define mp map<int,int>
+#define ump unordered_map<int,int>
+#define space ' '
+#define invi(n) vi arr(n); for(int i=0; i<n; i++) cin >> arr[i];
+#define stc stack<char>
+#define sti stack<int>
+#define all(arr) arr.begin(),arr.end()
+#define looprev(i,a,b) for(int i=a;i>=(b);i--)
+#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<space;std::cout<<std::endl;
+#define si set<int>
+int gCd(int a,int b){
+    if(b==0)
+        return a;
+    return gCd(b,a%b);
+}
+int fact(int n){
+    int res=1;
+    for(int i=2;i<=n;i++)
+        res = (res*i)%mod;
+    return res;
+}
+int nCr(int n,int r){
+    return fact(n)/ (fact(r)*fact(n-r));
+}
+    
+bool compare(pair<int,int> &one, pair<int,int> &two){
+    return one.second < two.second;
+}
+int32_t main(){
+    tester{
+        int n;cin>>n;
+        int temp=n;
+        vi arr;
+        int cnt=0;
+        while(n!=0){
+            int curr = n%10;n/=10;
+            if(curr==0)
+                cnt++;
+            arr.pb(curr);
+        }
+        if(arr.size()==1){
+            cout<<1<<endl;
+            cout<<temp<<endl;
+        }else{
+            cout<<arr.size()-cnt<<endl;
+            looper(i,0,arr.size()){
+                int curr = arr[i];
+                curr = curr*(pow(10,i));
+                if(curr==0){
+                    continue;
+                }
+                cout<<curr<<space;
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
